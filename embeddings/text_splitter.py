2b17f1c35f272eb0aa1ea5ebda450c15b176ba2a
Nishikanth23 <35811911+Nishikanth1@users.noreply.github.com>
2024-10-24 23:26:37 +0530

    add json file reader

---
+++ embeddings/text_splitter.py
@@ -0,0 +1,50 @@
+import os
+import sys
+import logging
+import json
+
+
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveJsonSplitter
+
+from file_loader import FileReader
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+logging.basicConfig(encoding='utf-8', format='%(asctime)s %(levelname)s  %(message)s', level=logging.INFO)
+
+class TextSplitter:
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.file_extension = os.path.splitext(filepath)[1].lower()
+        self.loader = FileReader(filepath)
+
+    def split(self):
+        if self.file_extension == '.pdf':
+            splitter = RecursiveCharacterTextSplitter(
+                                                            chunk_size=1000,
+                                                            chunk_overlap=200,
+                                                            length_function=len,
+                                                            is_separator_regex=False,
+                                                    )
+            for chunk in self.loader.read():
+                split = splitter.split_documents([chunk])
+                yield split            
+        elif self.file_extension == ".json":
+            splitter = RecursiveJsonSplitter(max_chunk_size=300)
+            for chunk in self.loader.read():
+                # TODO find better way to split json
+                parsed_chunk = json.loads(chunk.page_content)
+                split = splitter.split_text(parsed_chunk)
+                yield split
+                
+        else: 
+            raise ValueError(f"Unknown extension {self.file_extension} for text splitting")        
+
+if __name__ == "__main__":
+    splitter = TextSplitter("./data/documents/open_api.json")
+    print([l for l in splitter.split()])
+    
+    splitter = TextSplitter("./data/documents/Deloitte-SOC2.pdf")
+    print([l for l in splitter.split()])
+        
